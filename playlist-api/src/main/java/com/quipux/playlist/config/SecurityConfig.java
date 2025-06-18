7cc8f35a0773f8c05c12890e45fce56e8b9c988e
DeiviJohanCG <deivijohan9406@outlook.com>
2025-06-18 09:15:49 -0500

    Se crea el metodo "corsConfigurationSource" para la configuración de cors

--- playlist-api/src/main/java/com/quipux/playlist/config/SecurityConfig.java
+++ playlist-api/src/main/java/com/quipux/playlist/config/SecurityConfig.java
@@ -31,4 +31,17 @@ public class SecurityConfig {
 	        .cors(Customizer.withDefaults())
 	        .build();
 	}
+	
+	@Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
